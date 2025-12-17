464687e9d7241bc934e50e912e38fd70274d2457
Xuan Duy <moiduy04@gmail.com>
2025-12-17 16:55:47 +0700

    Add Qwen2.5-3B training script

---
+++ script/learn_mask_qwen_2.5_3b_prox.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+set -e
+
+# ------------ Hyperparams ------------ #
+model_dir=Qwen
+model_subdir=Qwen2.5-3B
+
+lr=1e-04
+ctx_len=4096
+per_device_train_batch_size=16
+lambda_=0.2
+
+# -------------- Constants -------------- #
+lr_float=$(printf "%g" "$lr")
+DIR="${model_subdir}-lr${lr_float}_len${ctx_len}_batch${per_device_train_batch_size}_lambda${lambda_}"
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
+
+# ---------------- MAIN ---------------- #
+echo -e "Starting learning mask with ProxSparse"
+
+CUDA_VISIBLE_DEVICES=7 PYTHONPATH="$PROJECT_ROOT" \
+python -m end_to_end.main \
+  --model_name "${model_dir}/${model_subdir}" \
+  --lambda_value $lambda_ \
+  --ctx_len $ctx_len \
+  --per_device_train_batch_size $per_device_train_batch_size \
+  --learning_rate $lr
+
+echo -e "Finished learning, now extracting binary mask. Mask stored in proximal_* directory"
+
+cd "$PROJECT_ROOT"
+CUDA_VISIBLE_DEVICES=7 PYTHONPATH="$PROJECT_ROOT" \
+python -m end_to_end.mask_op \
+  --mask_dir "$DIR" \
+  --ckpt "last"
+
+
