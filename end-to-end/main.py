e2b6d4499da50162746b231e0ceabc21f841fda1
Xuan Duy <moiduy04@gmail.com>
2025-12-17 14:25:39 +0700

    Refactor code to follow PEP8

--- end-to-end/main.py
+++ end-to-end/main.py
@@ -1,10 +1,11 @@
-SEED=1836
+SEED = 1836
 
 import argparse
-import torch
-import numpy as np
 import os
 import random
+
+import numpy as np
+import torch
 
 os.environ["TRL_USE_LIGER_KERNEL"] = "False"
 os.environ["HF_TRACKIO_DISABLE"] = "1"
@@ -19,29 +20,6 @@ from patch_transformers_trainer import prox_inner_training_loop, prox_compute_lo
 from prox_linear import replace_prox_linear
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--model', type=str, default="meta-llama/Llama-2-7b",
-                    help='Provide the model name for finetuning')
-parser.add_argument('--dataset', type=str, default="en",
-                    help='Calibration dataset')
-parser.add_argument('--lambda_value', type=float, default=0.1,
-                    help='lambda value')
-parser.add_argument('--lambda2_value', type=float, default=0.1,
-                    help='lambda2 value')
-parser.add_argument('--ctx_len', type=int, default="1024",
-                    help='ctx length ratio')
-parser.add_argument('--samples', type=int, default="3200",
-                    help='samples nr')
-parser.add_argument('--batch_size', type=int, default="1",
-                    help='batch size')
-parser.add_argument('--project_lambda2', type=int, default="0",
-                    help='projected descent')
-parser.add_argument('--epsilon', type=float, default="0.1",
-                    help='lambda2 epsilon')
-parser.add_argument('--learning_rate', type=float, default=1e-4,
-                    help='lr')
-
-
 def seed_everything(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
@@ -51,107 +29,131 @@ def seed_everything(seed):
     torch.backends.cudnn.deterministic = True
     torch.cuda.manual_seed_all(seed)
 
-seed_everything(SEED)
+
+def main():
+    seed_everything(SEED)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, default="meta-llama/Llama-2-7b",
+                        help='Provide the model name for finetuning')
+    parser.add_argument('--dataset', type=str, default="en",
+                        help='Calibration dataset')
+    parser.add_argument('--lambda_value', type=float, default=0.1,
+                        help='lambda value')
+    parser.add_argument('--lambda2_value', type=float, default=0.1,
+                        help='lambda2 value')
+    parser.add_argument('--ctx_len', type=int, default="1024",
+                        help='ctx length ratio')
+    parser.add_argument('--samples', type=int, default="3200",
+                        help='samples nr')
+    parser.add_argument('--batch_size', type=int, default="1",
+                        help='batch size')
+    parser.add_argument('--project_lambda2', type=int, default="0",
+                        help='projected descent')
+    parser.add_argument('--epsilon', type=float, default="0.1",
+                        help='lambda2 epsilon')
+    parser.add_argument('--learning_rate', type=float, default=1e-4,
+                        help='lr')
+
+    BASE_DIR = Path(__file__).resolve().parent.parent  # {etc}/ProxSparse_Test
+    args = parser.parse_args()
+    model_name = args.model
+    dataset_name = args.dataset
+    lr = args.learning_rate
+    ctx_len = args.ctx_len
+    samples = args.samples
+    batch_size = args.batch_size
+    epsilon = args.epsilon
+
+    config.lambda_ = args.lambda_value
+    config.epsilon = epsilon
+    config.lambda2_ = args.lambda2_value
+    config.project_lambda2 = args.project_lambda2
+
+    print(f'Loading dataset at {str(BASE_DIR.parent / "data" / "for_susi")}...', end=' ')
+    dataset = load_dataset(
+        "parquet",
+        data_files={
+            "train": str(BASE_DIR.parent / "data" / "for_susi" / "*.parquet")
+        },
+    )
+
+    train_testvalid = dataset["train"].train_test_split(test_size=0.95,
+                                                        seed=SEED)  # train 3563 on 0.99 # when data size is large, there might be memory outage, mostly 95
+    valid_test = train_testvalid["test"].train_test_split(test_size=0.999, seed=SEED)  # valid 352
+    dataset = DatasetDict({
+        'train': train_testvalid['train'].select(range(samples)),  # 5x data, but 16x, in case of huggingface bug
+        'validation': valid_test['train'].select(range(32)),  # we prune at every checkpoint
+        'test': valid_test['test']})
+
+    del train_testvalid, valid_test, dataset["test"]
+    print('Done')
+
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        torch_dtype=torch.bfloat16,  # float 32
+        device_map="auto",
+    )
+
+    if config.project_lambda2 == 1:
+        print("# begin replacing layers")
+        replace_prox_linear(model)
+        print("# replacing sucessful!")
+    model.train()
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.padding_side = "left"
+
+    dataset_id = dataset_name
+    BASE_MODEL = model_name
+
+    if config.project_lambda2 == 1:
+        repository_id = f"{BASE_MODEL.split('/')[1]}-{dataset_id}_sft_final_reg{regularizer}_{samples}_lr{lr}_len{ctx_len}_batch{batch_size}_lambdaone{config.lambda_}_lambdatwo{config.lambda2_}"
+    else:
+        repository_id = f"{BASE_MODEL.split('/')[1]}-{dataset_id}_sft_final_{samples}_lr{lr}_len{ctx_len}_batch{batch_size}_lambda{config.lambda_}"
+
+    # repository_path = BASE_DIR.parent.parent / "saved_models" / repository_id
+
+    sft_config = SFTConfig(
+        dataset_text_field="text",
+        output_dir=repository_id,
+        per_device_train_batch_size=batch_size,
+        per_device_eval_batch_size=batch_size,
+        max_length=ctx_len,
+        learning_rate=lr,
+        num_train_epochs=1,
+        optim="adamw_torch_fused",
+        warmup_ratio=0.1,
+        logging_dir=f"{repository_id}/logs",
+        logging_strategy="steps",
+        logging_steps=0.1,
+        logging_first_step=True,
+        eval_strategy="no",
+        # eval_steps=0.1,
+        # eval_accumulation_steps=2,
+        save_strategy="steps",
+        save_steps=0.1,
+        save_total_limit=None,
+        # load_best_model_at_end=True,
+        save_only_model=True,
+        report_to="none",
+    )
+
+    # patching with Proxsparse operator
+    print("Patching inner training loop and loss computation")
+    SFTTrainer._inner_training_loop = prox_inner_training_loop
+    SFTTrainer.compute_loss = prox_compute_loss
+
+    trainer = SFTTrainer(
+        model=model,
+        train_dataset=dataset["train"],
+        eval_dataset=dataset["validation"],
+        args=sft_config,
+    )
+
+    trainer.train()
+    print("Finished!")
 
 
-BASE_DIR = Path(__file__).resolve().parent.parent  # {etc}/ProxSparse_Test
-args = parser.parse_args()
-model_name = args.model
-dataset_name = args.dataset
-lr = args.learning_rate
-ctx_len = args.ctx_len
-samples = args.samples
-batch_size = args.batch_size
-epsilon = args.epsilon
-
-config.lambda_ = args.lambda_value
-config.epsilon = epsilon
-config.lambda2_ = args.lambda2_value
-config.project_lambda2 = args.project_lambda2
-
-print(f'Loading dataset at {str(BASE_DIR.parent / "data" / "for_susi")}...', end=' ')
-dataset = load_dataset(
-    "parquet",
-    data_files={
-        "train": str(BASE_DIR.parent / "data" / "for_susi" / "*.parquet")
-    },
-)
-
-train_testvalid = dataset["train"].train_test_split(test_size=0.95, seed=SEED)  # train 3563 on 0.99 # when data size is large, there might be memory outage, mostly 95
-valid_test = train_testvalid["test"].train_test_split(test_size=0.999, seed=SEED)  # valid 352
-dataset = DatasetDict({
-    'train': train_testvalid['train'].select(range(samples)),  # 5x data, but 16x, in case of huggingface bug
-    'validation': valid_test['train'].select(range(32)),  # we prune at every checkpoint
-    'test': valid_test['test']})
-
-del train_testvalid, valid_test, dataset["test"]
-print('Done')
-
-
-model = AutoModelForCausalLM.from_pretrained(
-    model_name,
-    torch_dtype=torch.bfloat16,  # float 32
-    device_map="auto",
-)
-
-if config.project_lambda2 == 1:
-    print("# begin replacing layers")
-    replace_prox_linear(model)
-    print("# replacing sucessful!")
-model.train()
-
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-tokenizer.pad_token = tokenizer.eos_token
-tokenizer.padding_side = "left"
-
-dataset_id = dataset_name
-BASE_MODEL = model_name
-
-if config.project_lambda2 == 1:
-    repository_id = f"{BASE_MODEL.split('/')[1]}-{dataset_id}_sft_final_reg{regularizer}_{samples}_lr{lr}_len{ctx_len}_batch{batch_size}_lambdaone{config.lambda_}_lambdatwo{config.lambda2_}"
-else:
-    repository_id = f"{BASE_MODEL.split('/')[1]}-{dataset_id}_sft_final_{samples}_lr{lr}_len{ctx_len}_batch{batch_size}_lambda{config.lambda_}"
-
-# repository_path = BASE_DIR.parent.parent / "saved_models" / repository_id
-
-
-sft_config = SFTConfig(
-    dataset_text_field="text",
-    output_dir=repository_id,
-    per_device_train_batch_size=batch_size,
-    per_device_eval_batch_size=batch_size,
-    max_length=ctx_len,
-    learning_rate=lr,
-    num_train_epochs=1,
-    optim="adamw_torch_fused",
-    warmup_ratio=0.1,
-    logging_dir=f"{repository_id}/logs",
-    logging_strategy="steps",
-    logging_steps=0.1,
-    logging_first_step=True,
-    eval_strategy="no",
-    # eval_steps=0.1,
-    # eval_accumulation_steps=2,
-    save_strategy="steps",
-    save_steps=0.1,
-    save_total_limit=10,
-    # load_best_model_at_end=True,
-    save_only_model=True,
-    report_to="none",
-)
-
-
-# patching with Proxsparse operator
-print("Patching inner training loop and loss computation")
-SFTTrainer._inner_training_loop = prox_inner_training_loop
-SFTTrainer.compute_loss = prox_compute_loss
-
-trainer = SFTTrainer(
-    model=model,
-    train_dataset=dataset["train"],
-    eval_dataset=dataset["validation"],
-    args=sft_config,
-)
-
-trainer.train()
-print("Finished!")
+if __name__ == "__main__":
+    main()
--- end-to-end/mask_op.py
+++ end-to-end/mask_op.py
@@ -1,11 +1,13 @@
-import evaluate
+SEED = 1836
 
-import datasets
+import argparse
+import os
 import random
+
 import numpy as np
 import torch
 from transformers import AutoModelForCausalLM
-import os
+
 
 def seed_everything(seed):
     torch.manual_seed(seed)
@@ -16,18 +18,15 @@ def seed_everything(seed):
     torch.backends.cudnn.deterministic = True
     torch.cuda.manual_seed_all(seed)
 
-seed_everything(42)
-perplexity = evaluate.load("perplexity", module_type="metric") # previously 0
-input_texts = datasets.load_dataset("allenai/c4", data_files="en/c4-train.00001-of-01024.json.gz", split="train")["text"][:3000]
 
-
-def explicit_sparsify_magnitude_row(weight, n, m): # prune and leave n out of m in 2d array
+def explicit_sparsify_magnitude_row(weight, n, m):  # prune and leave n out of m in 2d array
     dim = weight.shape
-    weight = weight.view(-1, m) # for a 2d array, reshale into 4 blocks
+    weight = weight.view(-1, m)  # for a 2d array, reshale into 4 blocks
     w_mask = torch.zeros_like(weight)
-    w_mask.scatter_(1, torch.topk(torch.abs(weight), n, dim = 1, largest = True)[1], True)
+    w_mask.scatter_(1, torch.topk(torch.abs(weight), n, dim=1, largest=True)[1], True)
     w_mask = w_mask.view(dim[0], dim[1])
     return w_mask
+
 
 def reshape_weights(weight_matrix):
     m, n = weight_matrix.shape
@@ -35,52 +34,54 @@ def reshape_weights(weight_matrix):
     return weight_matrix, m, n
 
 
-import argparse
+def main():
+    seed_everything(SEED)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, default="meta-llama/Meta-Llama-3-8B",
+                        help='Provide the model name for finetuning')
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--model', type=str, default= "meta-llama/Meta-Llama-3-8B", 
-                    help='Provide the model name for finetuning')
-parser.add_argument('--method', type=str, default= "proximal", 
-                    help='proximal')
+    args = parser.parse_args()
+    model_name = args.model
 
-args = parser.parse_args()
-model_name = args.model
-method = args.method
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        torch_dtype=torch.bfloat16,
+        device_map="auto",
+    )
+    one_sparse = 0
+    two_sparse = 0
+    three_sparse = 0
+    dense = 0
+    total = 0
 
-model = AutoModelForCausalLM.from_pretrained(
-    model_name,
-    torch_dtype=torch.bfloat16,
-    device_map= "auto",
-)
-one_sparse = 0 
-two_sparse = 0
-three_sparse = 0
-dense = 0
-total = 0
+    for n, m in model.named_parameters():
+        if "bias" not in n and (
+                "k_proj" in n or "q_proj" in n or "v_proj" in n or "o_proj" in n or "up_proj" in n or "down_proj" in n or "gate_proj" in n or "out_proj" in n or "fc1" in n or "fc2" in n):
+            mm, _, __ = reshape_weights(m)
+            total += mm.shape[0]
+            zero_counts = (mm == 0).sum(dim=1)
+            num_zeros_0 = (zero_counts == 0).sum().item()
+            num_zeros_1 = (zero_counts == 1).sum().item()
+            num_zeros_2 = (zero_counts == 2).sum().item()
+            num_zeros_3 = (zero_counts == 3).sum().item()
 
-for n, m in model.named_parameters():
-    if "bias" not in n and ("k_proj" in n or "q_proj" in n or "v_proj" in n or "o_proj" in n or "up_proj" in n or "down_proj" in n or "gate_proj" in n or "out_proj" in n or "fc1" in n or "fc2" in n):
-        mm, _, __ = reshape_weights(m)
-        total += mm.shape[0]
-        zero_counts = (mm == 0).sum(dim=1)
-        num_zeros_0 = (zero_counts == 0).sum().item()
-        num_zeros_1 = (zero_counts == 1).sum().item()
-        num_zeros_2 = (zero_counts == 2).sum().item()
-        num_zeros_3 = (zero_counts == 3).sum().item()
+            dense += num_zeros_0
+            one_sparse += num_zeros_1
+            two_sparse += num_zeros_2
+            three_sparse += num_zeros_3
 
-        dense += num_zeros_0
-        one_sparse += num_zeros_1
-        two_sparse += num_zeros_2
-        three_sparse += num_zeros_3
+            mask = explicit_sparsify_magnitude_row(m, 2, 4)
+            if not os.path.isdir(f"./proximal_{model_name.split('/')[0]}"):
+                os.mkdir(f"./proximal_{model_name.split('/')[0]}")
+            if not os.path.isdir(f"./proximal_{model_name}"):
+                os.mkdir(f"./proximal_{model_name}")
+            torch.save(mask, f"./proximal_{model_name}/{n}.pt")
 
-        mask = explicit_sparsify_magnitude_row(m, 2, 4)
-        if not os.path.isdir(f"./proximal_{model_name.split('/')[0]}"):
-            os.mkdir(f"./proximal_{model_name.split('/')[0]}")
-        if not os.path.isdir(f"./proximal_{model_name}"):
-            os.mkdir(f"./proximal_{model_name}")
-        torch.save(mask, f"./proximal_{model_name}/{n}.pt")
+    print("one sparse ratio: ", one_sparse / total)
+    print("two sparse ratio: ", two_sparse / total)
+    print("three sparse ratio: ", three_sparse / total)
+    print("dense ratio: ", dense / total)
 
-print("one sparse ratio: ", one_sparse/total)
-print("two sparse ratio: ", two_sparse/total)
-print("three sparse ratio: ", three_sparse/total)
-print("dense ratio: ", dense/total)
+
+if __name__ == "__main__":
+    main()
